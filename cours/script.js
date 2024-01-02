a33b8c2a6c7c1694d6b842efdc5f33086555e245
Kayzer <max.mlv2210@gmail.com>
2024-01-02 15:21:12 +0100

    Rappel des types de données

---
+++ cours/script.js
@@ -0,0 +1,7 @@
+// **************** Rappel des types de données **************** //
+
+// Si on met des ("") sa devient une chaine de carractère.
+let string = "chaine";
+let number = 25;
+let boolean = true;
+let maVariable = null; // Type Undefined
